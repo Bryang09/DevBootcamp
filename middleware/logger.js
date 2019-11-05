27fee171b2a4d6c1691ff34146ba0e36679ffc37
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2019-11-05 16:33:53 -0600

    Morgan Middleware Added

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+// @desc        LOGS REQUEST TO CONSOLE
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+};
+
+module.exports = logger;
