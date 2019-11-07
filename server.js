87f849e096ab64ea70aed441525174db0a528893
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2019-11-06 18:12:36 -0600

    Bootcamps model created

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // ROUTE FILES
 const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const app = express();
 
@@ -19,7 +22,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const port = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   port,
-  console.log(`Server Running in ${process.env.NODE_ENV} Mode on Port ${port}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} Mode on Port ${port}`.yellow
+  )
 );
+
+// Unhadled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
